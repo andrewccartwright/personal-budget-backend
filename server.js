43208e69e0fe82d56f813730f77f5358c0656f1e
andrewchatch <andrew.hatch96@gmail.com>
2022-04-06 14:55:26 -0600

    CORS origin

--- server.js
+++ server.js
@@ -7,13 +7,9 @@ const app = express();
 
 
 app.use(bodyParser.json());
-app.use(cors());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors({
+    origin: '*'
+}));
 
 const PORT = process.env.PORT || 8000;
 
