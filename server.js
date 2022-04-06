b23599271c4fcb28a3e2e18c50f553376122c2a5
andrewchatch <andrew.hatch96@gmail.com>
2022-04-06 14:06:56 -0600

    CORS policy

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const PORT = process.env.PORT || 8000;
 
 app.use('/expenses', expenseRouter);
