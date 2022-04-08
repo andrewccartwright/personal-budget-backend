390a5fccd33c077aef1c78d3a869bb9bff88ddcc
andrewchatch <andrew.hatch96@gmail.com>
2022-04-07 21:32:37 -0600

    Added CORS credentials

--- server.js
+++ server.js
@@ -49,8 +49,6 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
-console.log(GOOGLE_CLIENT_ID);
-
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
