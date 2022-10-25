e6ad6e3b827af0c4a7ab710943a3dba23c1463a7
andrewchatch <andrew.hatch96@gmail.com>
2022-10-25 16:28:33 -0600

    Update server.js

--- server.js
+++ server.js
@@ -17,65 +17,11 @@ app.use(cors({
     credentials: true
 }));
 
-// app.use(session({ secret: 'i like to eat pizza' }));
-
 app.use('/expenses', expenseRouter);
 
 app.use('/income', incomeRouter);
 
-// app.use('/', express.static('public'));
-
-// const oAuth2Client = new OAuth2Client();
-
-// var userProfile;
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// app.set('view engine', 'ejs');
-
-// app.get('/success', (req, res) => res.send(userProfile));
-// app.get('/error', (req, res) => res.send("error logging in"));
-
-// passport.serializeUser(function(user, cb) {
-//   cb(null, user);
-// });
-
-// passport.deserializeUser(function(obj, cb) {
-//   cb(null, obj);
-// });
-
-// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-// const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-
-// passport.use(new GoogleStrategy({
-//     clientID: GOOGLE_CLIENT_ID,
-//     clientSecret: GOOGLE_CLIENT_SECRET,
-//     callbackURL: "https://andrewchatch.github.io/personal-budget"
-//   },
-//   function(accessToken, refreshToken, profile, done) {
-//       userProfile=profile;
-//       console.log(userProfile);
-//       return done(null, userProfile);
-//   }
-// ));
- 
-// app.get('/auth/google', 
-//   passport.authenticate('google', { scope : ['profile', 'email'] }));
- 
-// app.get('/auth/google/callback', 
-//   passport.authenticate('google', { failureRedirect: '/error' }),
-//   function(req, res) {
-//     // Successful authentication, redirect success.
-//     res.redirect('/success');
-//   });
-
-
-
 const PORT = process.env.PORT || 8000;
-
-
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
